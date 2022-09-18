8e4207108375382f2113aa63491732bb0a059e7d
uditi-12 <uditi.gupta2020@vitstudent.ac.in>
2022-09-18 23:29:01 +0530

    files for lib

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/basic.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+void main() {
+  runApp(MyHomePage());
+}
+class MyHomePage extends StatefulWidget {
+  @override
+  myHomePageState createState() => myHomePageState();
+}
+class myHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyMi'),
+          ),
+          body: Center(child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(25),
+              child: TextButton(
+                child: Text('SignUp', style: TextStyle(fontSize: 20.0),),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(25),
+              child: TextButton(
+                child: Text('LogIn', style: TextStyle(fontSize: 20.0),),
+                // style: TextButton.styleFrom(
+                //   : Colors.blueAccent,
+                // textColor: Colors.white),
+                onPressed: () {},
+              ),
+            ),
+          ]
+          ))
+      ),
+      );
+  }
+}
+
+
